895929a615b06ace9f996cde3356b670dc300612
zandrest <edwin.villamil.laverde@hotmail.com>
2021-11-23 16:21:06 -0500

    mm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EjemploComponent } from './ejemplo/ejemplo.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EjemploComponent
+    EjemploComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
